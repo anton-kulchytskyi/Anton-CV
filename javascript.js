c592e7bca58b024407d78fbca0a9d6a6b750a079
Anton Kulchytskyi <anton@oykos-hub.com>
2020-05-02 14:15:27 +0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -7,3 +7,18 @@ function showHide(nav, hide1, hide2, hide3, hide4, hide5) {
         else obj.style.display = "none"; 
     }     
 }
+var modal = document.getElementById('modal');
+var close = document.getElementById('close');
+window.onload = function () {
+    setTimeout(function() {
+        modal.style.display = 'inline';
+    }, 30000)
+}
+close.addEventListener('click', function() {
+    modal.style.display = 'none';
+});
+window.addEventListener('click', function(e) {
+    if(e.target == modal) {
+        modal.style.display = 'none';
+    }
+});
